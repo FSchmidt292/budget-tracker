b3da0899cff63d719a7c401ae23764ad9cc13037
Trey S <fjschmidt292@gmail.com>
2022-07-04 23:31:53 -0400

    templating out IDB

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,2 +1,19 @@
 let db;
-const request = indexedDB.open('budget')
+const request = indexedDB.open('budget', 1)
+
+request.onupgradedneeded = function(event) {
+    const db = event.target.result;
+    db.createObjectStore('new_budget', { autoIncrement: true});
+};
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+
+    if (navigator.onLine) {
+        uploadBudget();
+    }
+};
+
+request.onerror = function(event) {
+    console.log(event.target.errorCode);
+};
